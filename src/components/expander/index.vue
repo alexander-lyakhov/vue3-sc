ba8259e38a1bc49aaca50bcc055f5c8fc8559588
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2020-11-04 18:33:35 +0200

    created empty page with aside

---
+++ src/components/expander/index.vue
@@ -0,0 +1,15 @@
+﻿<template>
+  <div class="expander">
+    <div class="expander-header">
+      Filter
+    </div>
+    <div class="expander-body">
+    </div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+</style>
