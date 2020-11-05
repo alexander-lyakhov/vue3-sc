816891995e4ba2bad7b47b47a5260a778f062f69
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2020-11-05 18:01:22 +0200

    implemented custom checkbox

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
   <main>
     <aside>
+      <expander>
+        <sc-checkbox value="1" name="brands" v-model="brands">one</sc-checkbox>
+        <sc-checkbox value="2" name="brands" v-model="brands">two</sc-checkbox>
+        <sc-checkbox value="3" name="brands" v-model="brands">three</sc-checkbox>
+        <sc-checkbox value="4" name="brands" v-model="brands">four</sc-checkbox>
+      </expander>
     </aside>
   </main>
 </template>
+
+<script>
+import expander from '@/components/expander';
+import scCheckbox from '@/components/global/sc-checkbox';
+
+export default {
+  components: {
+    expander,
+    scCheckbox
+  },
+
+  data() {
+    return {
+      brands: []
+    }
+  },
+
+  created() {
+    console.log('brands', this.brands)
+  },
+
+  methods: {
+    onChange(e) {
+      console.log('onChange', e)
+    }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 main {
@@ -13,10 +47,9 @@ main {
 }
 
 aside {
-  background: #f8f8f8;
+  background: #fff;
   box-shadow: 0 6px 8px rgba(0,0,0,0.4);
   border: 1px solid transparent;
-  width: 272px;
-  height: 100vh;
+  width: 288px;
 }
 </style>
--- src/components/expander/index.vue
+++ src/components/expander/index.vue
@@ -1,15 +1,70 @@
 ﻿<template>
-  <div class="expander">
-    <div class="expander-header">
+  <div class="expander noselect">
+    <div class="expander-header" @click="toggleShow">
       Filter
     </div>
-    <div class="expander-body">
+    <div class="expander-body" v-show="isOpen">
+      <slot></slot>
     </div>
   </div>
 </template>
 
 <script>
+
+export default {
+  name: 'Expander',
+
+  data() {
+    return {
+      isOpen: true
+    }
+  },
+  methods: {
+    toggleShow() {
+      this.isOpen = !this.isOpen
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped>
+
+.expander {
+  color: #000;
+  background: #fff;
+  box-sizing: border-box;
+  width: 256px;
+  margin: 1rem auto;
+
+  &-header {
+    font-weight: bold;
+    border-bottom: 1px solid #ccc;
+    box-sizing: content-box;
+    height: 2rem;
+    line-height: 2rem;
+    display: block;
+    position: relative;
+    margin: 2px;
+    cursor: pointer;
+
+    &:after {
+      content: '+';
+      font-size: 1.5rem;
+      font-weight: normal;
+      text-align: center;
+      display: block;
+      width: 2rem;
+      height: 2rem;
+      position: absolute;
+      top: 0;
+      right: 0;
+    }
+  }
+
+  &-body {
+    font-size: .875rem;
+    margin: 2px;
+    padding: .5rem 0;
+  }
+}
 </style>
