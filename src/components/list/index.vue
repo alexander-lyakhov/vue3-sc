2aad9497731619f12e0e7c73564ae5fce9acac4c
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2020-11-07 20:30:25 +0200

    added list component

---
+++ src/components/list/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <ul>
+    <slot></slot>
+  </ul>
+  <div class="show-more">Show more</div>
+</template>
+
+<script>
+
+export default {
+  name:'list';
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
