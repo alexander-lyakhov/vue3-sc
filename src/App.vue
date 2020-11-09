18696b6fa2c4f04284201433aaf381dc14ad04b2
Alexander Lyakhov <alexr.lyakhov@gmail.com>
2020-11-09 22:55:05 +0200

    added breadcrumb for Category

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,23 @@
   <main>
     <aside>
       <expander v-for="(facet, facetIndex) in listFacets" :key="facetIndex" :title="facet.title">
-        <template v-if="facet.moduleType === 'FacetsTile'">
-          <list :items="facet.values" :columns="getColomnCount(facet)">
-            <template v-if="facet.type === 'breadcrumbed'" #default="{item}">
-              <a href="#">
-                {{item.value}}
-              </a>
-              <span class="count">({{item.count}})</span>
-            </template>
+        <template v-if="isBreadcrumbed(facet)">
+          <div class="breadcrumbs">
+            <list :items="facet.breadcrumbs" #default="{item}">
+              {{item.value}}
+            </list>
+          </div>
+          <list :items="facet.values" #default="{item}">
+            <a class="breadcrumb-link" href="#">
+              {{item.value}}
+            </a>
+            <span class="count">({{item.count}})</span>
+          </list>
+        </template>
 
-            <template v-else #default="{item}">
+        <template v-if="isCheckList(facet)">
+          <list :items="facet.values" :columns="getColomnCount(facet)">
+            <template #default="{item}">
               <sc-checkbox-group :value="item.value" :name="facet.title" v-model="filter[facet.title]">
                 {{item.value}}
               </sc-checkbox-group>
@@ -20,7 +27,7 @@
           </list>
         </template>
 
-        <template v-if="facet.moduleType === 'FacetsColour'">
+        <template v-if="isColor(facet)">
           <color-palette :items="facet.values" />
         </template>
       </expander>
@@ -57,12 +64,24 @@ export default {
     listFacets() {
       //return Object.values(this.facets).filter(el => el.type === 'list');
       return this.facets;
-    }
+    },
   },
 
   methods: {
     getColomnCount(facet) {
       return facet.title.toLowerCase() === 'size'? 2:1;
+    },
+
+    isBreadcrumbed(facet) {
+      return facet.moduleType === 'FacetsTile' && facet.type === 'breadcrumbed';
+    },
+
+    isCheckList(facet) {
+      return facet.moduleType === 'FacetsTile' && facet.type === 'list';
+    },
+
+    isColor(facet) {
+      return facet.moduleType === 'FacetsColour';
     }
   }
 }
@@ -80,6 +99,44 @@ aside {
   box-shadow: 0 6px 8px rgba(0,0,0,0.4);
   border: 1px solid transparent;
   width: 336px;
+}
+
+.breadcrumbs {
+  background: #f0f0f0;
+  margin: 0 -0.5rem;
+  padding: 0 0.5rem;
+
+  &::v-deep li {
+    position: relative;
+
+    &:nth-child(n + 2):after {
+      content: '';
+      border-left: 1px solid #08c;
+      border-bottom: 1px solid #08c;
+      display: block;
+      width: .5rem;
+      height: .875rem;
+      position: absolute;
+      left: calc(-1rem + 2px);
+      top: 0;
+    }
+
+    &:nth-child(n + 2) {
+      margin-left: 1rem;
+    }
+
+    &:nth-child(n + 3) {
+      margin-left: 2rem;
+    }
+
+    &:nth-child(n + 4) {
+      margin-left: 3rem;
+    }
+  }
+}
+
+.breadcrumb-link:hover {
+  text-decoration: underline!important;
 }
 
 .count {
